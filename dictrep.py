164fb1a06b5204c99c6f7b3a2cb8eee004ae9b38
Srija U <48713553+Srija-U@users.noreply.github.com>
2019-04-05 10:02:47 +0430

    Update dictrep.py

--- dictrep.py
+++ dictrep.py
@@ -1,21 +1,21 @@
 n=int(input())
-a=[int(i) for i in input().split()]
-l=len(a)
-a.sort()
+ab=[int(i) for i in input().split()]
+l=len(ab)
+ab.sort()
 b={}
 c=[]
 t=[]
 te=0
 for i in range(0,l):
-    if(a[i] in b):
+    if(ab[i] in b):
         next
     else:
-        b[a[i]]=0
-        c.append(a[i])
+        b[ab[i]]=0
+        c.append(ab[i])
 le=len(c)
 for i in range(0,le):
     for j in range(0,l):
-        if(c[i]==a[j]):
+        if(c[i]==ab[j]):
             b[c[i]]+=1
 d=list(b.values())
 #c-->key_list
