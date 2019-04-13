b470d97b95d901c53caf77f9ca5bfcc04ca67505
Srija U <48713553+Srija-U@users.noreply.github.com>
2019-04-13 14:24:56 +0430

    Update set18.py

--- set18.py
+++ set18.py
@@ -4,4 +4,4 @@ for i in range(0,len(l)-2):
     for j in range(i+1,len(l)-1):
         for k in range(j+1,len(l)):
             if(l[i]+l[j]==l[k]):
-                print(l[i],l[j],l[k])
+                print(l[i],l[j],l[k],end="\n")
