537a1d477733c954b8fa3dcc9342b70cc9d2ba59
Srija U <48713553+Srija-U@users.noreply.github.com>
2019-04-13 14:45:34 +0430

    Update set19.py

--- set19.py
+++ set19.py
@@ -2,5 +2,5 @@ n=int(input())
 l=[int(i) for i in input().split()]
 for i in range(0,len(l)):
     for j in range(i+1,len(l)):
-        if(round(l[i]+l[j])==0):
+        if((round(l[i]+l[j])==0)or(round(l[i]+l[j])==-1)or(round(l[i]+l[j])==1)):
             print(l[i],l[j])
