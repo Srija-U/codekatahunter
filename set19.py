03a11fa2d9bd11c0da1ec1cb934ead27cb40e87d
Srija U <48713553+Srija-U@users.noreply.github.com>
2019-04-13 14:47:21 +0430

    Update set19.py

--- set19.py
+++ set19.py
@@ -4,3 +4,4 @@ for i in range(0,len(l)):
     for j in range(i+1,len(l)):
         if((round(l[i]+l[j])==0)or(round(l[i]+l[j])==-1)or(round(l[i]+l[j])==1)):
             print(l[i],l[j])
+            exit()
