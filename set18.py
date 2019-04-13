f516610abc67cffeb77e337389e757000fca06d8
Srija U <48713553+Srija-U@users.noreply.github.com>
2019-04-13 14:22:10 +0430

    Create set18.py

---
+++ set18.py
@@ -0,0 +1,7 @@
+n=int(input())
+l=[int(i) for i in input().split()]
+for i in range(0,len(l)-2):
+    for j in range(i+1,len(l)-1):
+        for k in range(j+1,len(l)):
+            if(l[i]+l[j]==l[k]):
+                print(l[i],l[j],l[k])
