b54be5282e3c6f524cad373cac05266e9c036120
Srija U <48713553+Srija-U@users.noreply.github.com>
2019-04-15 08:59:40 +0430

    Create set41.py

---
+++ set41.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=[int(i) for i in input().split()]
+a=[]
+for i in range(0,n):
+    c=1
+    for j in range(i,n):
+        c=c*l[j]
+    a.append(c)
+for i in range(0,n):
+    c=1
+    for j in range(i+1):
+        c=c*l[j]
+    a.append(c)
+print(max(a))
